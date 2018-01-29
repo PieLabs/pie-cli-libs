90d002bc00e044c5d1bcc10538fcd091184582c5
edeustace <ed.eustace@gmail.com>
2018-01-29 10:53:59 -0500

    fix(build): update build

--- packages/installer/lib/index.d.ts
+++ packages/installer/lib/index.d.ts
@@ -1,3 +1,4 @@
-import { ElementMap, Model, InstalledElement } from './installer';
+import { ElementMap, Model, InstalledElement, PreInstallRequest, PostInstall, PieInfo, Input } from './installer';
 import { Reporter } from './reporter';
+export { InstalledElement, PreInstallRequest, PostInstall, Input, PieInfo, Reporter };
 export declare function install(dir: string, elements: ElementMap, models: Model[], reporter: Reporter): Promise<InstalledElement[]>;
